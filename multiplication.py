9b65de19f87914dba2800e315ff1ce925ed5d12b
Vickystarq <111752370+Vickystarq@users.noreply.github.com>
2024-12-30 22:06:45 +0100

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# A code to generate a multiplication table from 1-12
+Num = 12 # defined value
+for i in range (1, Num+1):
+    for j in range(1, Num+1):
+        print(f"{i} * {j} = {i * j} \t \t")
+        print("\n")
+
+#End Code for multiplication Table
+        
